f6dccd4f4e5208f8401f679615f38cead841ba12
kirilok11 <123757857+kirilok11@users.noreply.github.com>
2023-06-18 23:07:25 +0200

    Delete Constants.cs

--- Constants.cs
+++
@@ -1,8 +0,0 @@
-﻿namespace API
-{
-    public class Constants
-    {
-        public static string apikey = "AIzaSyC6iS1WD4eOqKa8SLTWEto9za2Lumc0gvA";
-        public static string address = "https://maps.googleapis.com";
-    }
-}
